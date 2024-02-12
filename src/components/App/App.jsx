ab82409174aa0ca18364ca7b924808a2d8b84b3b
Anastasia Romanyuk <vnstyv@gmail.com>
2024-02-12 18:46:52 +0200

    ed3

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import userData from "../userData.json";
+import { Profile } from "../Profile/Profile";
+import friends from "../friends.json";
+import { FriendList } from "../FriendList/FriendList";
+import transactions from "../transactions.json";
+import { TransactionHistory } from "../TransactionHistory/TransactionHistory";
+
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+        />
+          <FriendList friends={friends} />
+          <TransactionHistory items={transactions} />
+          </>
+  );
+};
+
